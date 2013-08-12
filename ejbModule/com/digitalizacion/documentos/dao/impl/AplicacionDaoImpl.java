186604d862f710f61f66deed9e4c91b8d877b32d
jmenamontano <juanito17287@gmail.com>
2013-08-11 21:46:04 -0700

    Cambios Por pruebas

--- ejbModule/com/digitalizacion/documentos/dao/impl/AplicacionDaoImpl.java
+++ ejbModule/com/digitalizacion/documentos/dao/impl/AplicacionDaoImpl.java
@@ -15,7 +15,9 @@ public class AplicacionDaoImpl extends GenericDaoImpl<Aplicacion, Long> implemen
 	public AplicacionDaoImpl() {
 		super(Aplicacion.class);
 	}
-
+/**
+ * Prueba de SVN
+ */
 	public Aplicacion buscarPorCodigo(String codigoAplicacion) {
 		List<Aplicacion> aplicaciones;
 		aplicaciones = findByNamedQueryNamedParameter("Aplicacion.findByCodigo", new NamedQueryParameter("apliCodigo",
